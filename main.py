3cb47f5d1cb23bb4825807ef8a233fa0ce201c9f
melody <sjh855210@qq.com>
2025-12-30 11:27:36 +0800

    新建项目，测试是否能否正常提交

--- main.py
+++ main.py
@@ -5,3 +5,4 @@ c = (
 )  # Create a blank component (essentially an empty GDS cell with some special features)
 p1 = c.add_polygon([(-8, -6), (6, 8), (7, 17), (9, 5)], layer=(1, 0))
 c.show()  # show it in klayout
+
